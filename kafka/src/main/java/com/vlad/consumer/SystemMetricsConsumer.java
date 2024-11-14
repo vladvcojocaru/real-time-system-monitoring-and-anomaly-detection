af758183b976b4048467ca3203be300445998fcf
VladCojo <vladvcojocaru@gmail.com>
2024-11-14 10:53:11 +0200

    implemented basic file structure

---
+++ kafka/src/main/java/com/vlad/consumer/SystemMetricsConsumer.java
@@ -0,0 +1,4 @@
+package com.vlad.consumer;
+
+public class SystemMetricsConsumer {
+}
---
+++ kafka/src/main/java/com/vlad/producer/SystemMetricsProducer.java
@@ -0,0 +1,7 @@
+package com.vlad.producer;
+
+public class SystemMetricsProducer {
+    public static void main(String[] args) {
+        System.out.println("Hello world");
+    }
+}
