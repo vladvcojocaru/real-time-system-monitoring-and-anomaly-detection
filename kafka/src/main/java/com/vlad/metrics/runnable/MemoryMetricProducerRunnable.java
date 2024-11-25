a12ddcf58f7fa7a7f5c31b4357ce62f10d26380f
vlad cojocaru <vladvcojocaru@gmail.com>
2024-11-25 09:26:53 +0200

    feat: add MemoryMetricProducerRunnable to continuously produce memory metrics

---
+++ kafka/src/main/java/com/vlad/metrics/runnable/MemoryMetricProducerRunnable.java
@@ -0,0 +1,29 @@
+package com.vlad.metrics.runnable;
+
+import com.vlad.metrics.kafka.MemoryMetricProducer;
+import com.vlad.metrics.models.MemoryMetric;
+import com.vlad.metrics.services.MemoryMetricService;
+
+public class MemoryMetricProducerRunnable implements Runnable{
+    private final MemoryMetricProducer memoryMetricProducer;
+    private final MemoryMetricService memoryMetricService;
+
+    public MemoryMetricProducerRunnable(MemoryMetricProducer memoryMetricProducer, MemoryMetricService memoryMetricService) {
+        this.memoryMetricProducer = memoryMetricProducer;
+        this.memoryMetricService = memoryMetricService;
+    }
+
+
+    @Override
+    public void run() {
+        try {
+            while (true){
+                MemoryMetric memoryMetric = memoryMetricService.getMemoryMetrics();
+
+                memoryMetricProducer.sendMetric(memoryMetric);
+
+                Thread.sleep(1000);
+            }
+        }
+    }
+}
