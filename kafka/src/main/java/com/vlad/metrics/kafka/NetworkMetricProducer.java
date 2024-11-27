b96a411fa0e8b61796e1c345f8b7a4743e6c6427
vlad cojocaru <vladvcojocaru@gmail.com>
2024-11-27 09:58:38 +0200

    feat: add NetworkMetricProducer for producing network metrics

---
+++ kafka/src/main/java/com/vlad/metrics/kafka/NetworkMetricProducer.java
@@ -0,0 +1,15 @@
+package com.vlad.metrics.kafka;
+
+import com.vlad.metrics.models.DiskMetric;
+import com.vlad.metrics.models.NetworkMetric;
+
+public class NetworkMetricProducer extends MetricProducer<NetworkMetric>{
+    public NetworkMetricProducer(String topic) {
+        super(topic);
+    }
+
+    @Override
+    protected String serializeMetric(NetworkMetric metric) {
+        return metric.toJson();
+    }
+}
