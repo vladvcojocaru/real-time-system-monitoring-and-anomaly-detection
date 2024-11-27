55f941e9b725c9cbc2bcf794e6368d039b981013
vlad cojocaru <vladvcojocaru@gmail.com>
2024-11-27 09:48:42 +0200

    feat: add NetworkMetricService for collecting network metrics

---
+++ kafka/src/main/java/com/vlad/metrics/services/NetworkMetricService.java
@@ -0,0 +1,40 @@
+package com.vlad.metrics.services;
+
+import com.vlad.metrics.models.NetworkMetric;
+import oshi.SystemInfo;
+import oshi.hardware.NetworkIF;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class NetworkMetricService {
+    private final SystemInfo systemInfo;
+
+    public NetworkMetricService() {
+        systemInfo = new SystemInfo();
+    }
+
+    public NetworkMetric[] getNetworkMetric(){
+        List<NetworkIF> networkIFs = systemInfo.getHardware().getNetworkIFs();
+        ArrayList<NetworkMetric> networkMetricArrayList = new ArrayList<>();
+
+        for(NetworkIF networkIF: networkIFs){
+            networkIF.updateAttributes();
+
+            String name = networkIF.getName();
+            String displayName = networkIF.getDisplayName();
+            long bytesSent = networkIF.getBytesSent();
+            long bytesRecv = networkIF.getBytesRecv();
+            long packetsSent = networkIF.getPacketsSent();
+            long packetsRecv = networkIF.getPacketsRecv();
+            String[] ipv4Addresses = networkIF.getIPv4addr();
+            String[] ipv6Addresses = networkIF.getIPv6addr();
+            String macAddress = networkIF.getMacaddr();
+
+            networkMetricArrayList.add(new NetworkMetric(name, displayName, bytesSent, bytesRecv, packetsSent, packetsRecv, ipv4Addresses, ipv6Addresses, macAddress));
+        }
+
+        return networkMetricArrayList.toArray(new NetworkMetric[0]);
+
+    }
+}
