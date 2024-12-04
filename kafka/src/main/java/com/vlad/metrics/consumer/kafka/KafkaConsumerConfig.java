9e81171ab59ab48ba17d04bf3eb4506e7233f745
Vlad Cojocaru <vladvcojocaru@gmail.com>
2024-12-04 11:08:38 +0200

    create KafkaConsumerConfig class

---
+++ kafka/src/main/java/com/vlad/metrics/consumer/kafka/KafkaConsumerConfig.java
@@ -0,0 +1,33 @@
+package com.vlad.metrics.consumer.kafka;
+
+import java.util.Properties;
+import org.apache.kafka.clients.consumer.ConsumerConfig;
+import org.apache.kafka.clients.consumer.KafkaConsumer;
+
+public class KafkaConsumerConfig {
+
+    public static KafkaConsumer<String, String> createConsumer(String group) {
+        // The Kafka broker(s) to connect to. 'localhost:9092' assumes Kafka is running locally.
+        Properties properties = new Properties();
+        properties.setProperty(
+            ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG,
+            "localhost:9092"
+        );
+        properties.setProperty(ConsumerConfig.GROUP_ID_CONFIG, group);
+        properties.setProperty(
+            ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG,
+            "org.apache.kafka.common.serialization.StringDeserializer"
+        );
+        properties.setProperty(
+            ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG,
+            "org.apache.kafka.common.serialization.StringDeserializer"
+        );
+        properties.setProperty(
+            ConsumerConfig.AUTO_OFFSET_RESET_CONFIG,
+            "earliest"
+        );
+
+        // Return a new KafkaProducer instance with the above properties
+        return new KafkaConsumer<>(properties);
+    }
+}
